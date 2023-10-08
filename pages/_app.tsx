90e388490650858e54267c14b2e66af9df34cacd
Karem Shabana <km425360@gmail.com>
2023-10-09 00:54:22 +0300

    add meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,21 +3,34 @@ import MainLayout from "@/components/MainLayout";
 
 import "@/styles/custom.scss";
 import "@/styles/globals.scss";
-import "bootstrap-icons/font/bootstrap-icons.css"
+import "bootstrap-icons/font/bootstrap-icons.css";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-   const router = useRouter();
-   useEffect(() => {
-     typeof document !== undefined
-       ? require("bootstrap/dist/js/bootstrap")
-       : null;
-   }, [router.events]);
+  const router = useRouter();
+  useEffect(() => {
+    typeof document !== undefined
+      ? require("bootstrap/dist/js/bootstrap")
+      : null;
+  }, [router.events]);
 
   return (
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
+    <>
+      <Head>
+        <title>Little Lemon</title>
+        <meta name="description" content="Little Lemon Restaurant" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/assets/logo.svg" />
+        <meta property="og:title" content="Little Lemon" />
+        <meta property="og:description" content="Little Lemon Restaurant" />
+        <meta property="og:image" content="/assets/restaurant chef B.jpg" />
+        <meta property="og:url" content="https://www.littlelemon.com" />
+      </Head>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </>
   );
 }
--- pages/reservation.tsx
+++ pages/reservation.tsx
@@ -1,7 +1,21 @@
+import Head from "next/head";
 import React from "react";
 
 const TableReservation = () => {
-  return <div>TableReservation</div>;
+  return (
+    <>
+      <Head>
+        <title>Little Lemon | Table Reservation</title>
+        <meta name="description" content=" Little Lemon restaurant" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <>
+      </>
+
+    </>
+  );
 };
 
 export default TableReservation;
